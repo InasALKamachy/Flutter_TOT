aa869711c28f4240291d72f476b550760f6ddf2f
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-07-07 12:15:47 +0300

    Create onbording.dart

---
+++ onBording/onbording.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class OnBording extends StatefulWidget {
+  //just type stf then tab button
+
+  @override
+  _OnBordingState createState() => _OnBordingState();
+}
+
+class _OnBordingState extends State<OnBording> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        PageView.builder(
+          itemBuilder: (context, index) {
+            return Stack(
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  //decoration: BoxDecoration(
+                  //image: DecorationImage(
+                  // image: ExactAssetImage('assets/images/news.png'),
+                  //fit:BoxFit.cover,
+                  // ),
+                  //),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Transform.translate
+                      (child: Icon(
+                        Icons.ac_unit,
+                        size: 150,
+                        color: Colors.white),
+                    offset:Offset(0,-100)),
+                    Text(' Welcome ',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24)),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 48, left: 48),
+                      child: Text(
+                        'Machine Learning mobile application for users',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ); //return end with ;
+          },
+          itemCount: 4,
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 24),
+            child: RaisedButton(
+              color: Colors.red.shade800,
+              child: Text(
+                'Get Start',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        )
+      ],
+    ));
+  }
+}

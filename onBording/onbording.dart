d66f6fa1d001a98e3058e4f8f450235224bd08a2
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-07-09 21:35:24 +0300

    Update onbording.dart

--- onBording/onbording.dart
+++ onBording/onbording.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class OnBording extends StatefulWidget {
   //just type stf then tab button
 
@@ -8,9 +9,36 @@ class OnBording extends StatefulWidget {
 }
 
 class _OnBordingState extends State<OnBording> {
+  List<String> images = [
+    'assets/images/1.png',
+    'assets/images/2.png',
+    'assets/images/3.png',
+    'assets/images/4.png',
+  ];
+  List<IconData> icons = [
+    Icons.ac_unit,
+    Icons.access_alarm,
+    Icons.print,
+    Icons.map
+  ];
+
+  List<String> texts = [
+    'Welcome',
+    'Time',
+    'Print',
+    'Map',
+  ];
+
+  List<String> texts_sub = [
+    'Welcome to our mobile application in 2021',
+    'Time to our mobile application in 2021',
+    'Print to our mobile application in 2021',
+    'Map to our mobile application in 2021',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
         body: Stack(
       children: <Widget>[
         PageView.builder(
@@ -18,35 +46,40 @@ class _OnBordingState extends State<OnBording> {
             return Stack(
               children: <Widget>[
                 Container(
-                  color: Colors.red,
-                  //decoration: BoxDecoration(
-                  //image: DecorationImage(
-                  // image: ExactAssetImage('assets/images/news.png'),
-                  //fit:BoxFit.cover,
-                  // ),
-                  //),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: ExactAssetImage(images[index]),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Transform.translate
-                      (child: Icon(
-                        Icons.ac_unit,
-                        size: 150,
-                        color: Colors.white),
-                    offset:Offset(0,-100)),
-                    Text(' Inas Al-Kamachy ',
+                    Transform.translate(
+                        child:
+                            Icon(icons[index],
+                                size: 150,
+                                color: Colors.white),
+                        offset: Offset(0, -100)),
+                    Text(texts[index],
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24)),
+                          color: Colors.white,
+                          fontSize: 24,
+                        )),
                     Padding(
-                      padding: const EdgeInsets.only(right: 48, left: 48),
+                      padding: const EdgeInsets.only(
+                        right: 48,
+                        left: 48,
+                        top: 18,
+                      ),
                       child: Text(
-                        'Machine Learning mobile application for users',
+                        texts_sub[index],
                         style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 16),
+                          color: Colors.white70,
+                          fontSize: 16,
+                        ),
                         textAlign: TextAlign.center,
                       ),
                     ),
@@ -55,19 +88,30 @@ class _OnBordingState extends State<OnBording> {
               ],
             ); //return end with ;
           },
-          itemCount: 4,
+          itemCount: images.length,
         ),
+
+
+
         Align(
           alignment: Alignment.bottomCenter,
           child: Padding(
-            padding: const EdgeInsets.only(bottom: 24),
-            child: RaisedButton(
-              color: Colors.red.shade800,
-              child: Text(
-                'Get Start',
-                style: TextStyle(color: Colors.white),
+            padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
+            child: SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: RaisedButton(
+                color: Colors.red.shade800,
+                child: Text(
+                  'Get Start',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    letterSpacing: 1,
+                  ),
+                ),
+                onPressed: () {},
               ),
-              onPressed: () {},
             ),
           ),
         )
@@ -75,3 +119,4 @@ class _OnBordingState extends State<OnBording> {
     ));
   }
 }
+

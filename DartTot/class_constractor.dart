0ee361ae396b750c6fe0644a5c8ee28245abc406
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-07-12 15:20:45 +0300

    Create class_constractor.dart

---
+++ DartTot/class_constractor.dart
@@ -0,0 +1,95 @@
+class Person {
+  String name;
+
+  int age;
+  double salary;
+
+  String city;
+  String country;
+
+
+  Person(this.name, this.age, this.salary, this.city, this.country);
+
+
+
+
+
+  //  Person(String name, int age, double salary, String city, String country){
+//    this.name = name;
+//    this.age = age;
+//    this.salary = salary;
+//    this.city = city;
+//    this.country = country;
+//  }
+
+  void eatFun() {
+    print("${name} is eating");
+  }
+
+  void sleepFun() {
+    print("${name} is sleeping");
+  }
+
+  void walkFun() {
+    print("${name} is walking");
+  }
+
+  int ageStatus() => age;
+
+  String nameFun() => name;
+
+  bool status() => true;
+
+  set setAge(int value) => age = value;
+  int get getAge => age +9;
+//setter and getter
+  set setName(String value) => name = value;
+  String get getName => name;
+
+}
+
+
+main(List<String> arguments) {
+
+  var  person1 = new Person('inas', 34 , 4000.4 , 'baghdad' , 'iraq');
+  var  person2 = new Person('hyder', 37 , 2000.4 , 'sydia' , 'iraq');
+
+ /* person2.setAge = 99;
+  print(person2.getAge);
+  person2.setName = 'Waleed';
+  print(person2.getName);*/
+
+print(person1.name);
+//  var person3 = new Person.initialize();
+//  print(person3.age);
+//  print(person3.name);
+//  person3.name = 'Jabbar';
+//  print(person3.name);
+//  person1.name = "Muhammed Essa";
+//  person1.salary = 3000.0;
+//  person1.age = 34;
+//   person1.city = "Babil";
+//  person1.country = "IRAQ";
+
+//
+//  person2.name = "Ahmed Essa";
+//  person2.salary = 4000.0;
+//  person2.age = 24;
+//  person2.city = "Basra";
+//  person2.country = "IRAQ";
+
+
+
+//  person1.eatFun();
+//  person1.sleepFun();
+//  person1.walkFun();
+//
+//  print(person1.ageStatus());
+//  var fullname = person1.nameFun();
+//  print(fullname);
+//  print(person1.status());
+//
+//  print("${person1.name}  ${person1.salary}  ${person1.age}  ${person1.city}     ");
+//  print("${person2.name}  ${person2.salary}  ${person2.age}  ${person2.city}     ");
+
+}

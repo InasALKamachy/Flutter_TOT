8f0afde8e93cac6378734eb480a6483aa4494cc5
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-07-14 18:48:28 +0300

    Create list.dart

---
+++ DartTot/list.dart
@@ -0,0 +1,19 @@
+main(){
+  var l=[3,4,7,2];
+  print(l);
+  print(l.length);
+  print(l[0]);
+  print(l.first);
+  print(l.last);
+  print(l.reversed);
+  l.addAll([2,3,4]);
+  print(l);
+  l.insert(0,8);
+  print(l);
+  l.insertAll(0, [10,60,699]);
+  print(l);
+  l.remove(5);
+  print(l);
+
+
+}

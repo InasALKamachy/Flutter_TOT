08ca5060ec8109fb1d4beae94ba09cb11561ec33
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-07-15 09:07:33 +0300

    Create 2dlist.dart

---
+++ DartTot/2dlist.dart
@@ -0,0 +1,16 @@
+main(){
+  var l = [[1,2,3],[3,4,0]];
+  print(l);
+  for(var i=0; i<=1; i++){
+    for(var j=0; j<=2; j++){
+      print(l[i][j]);
+    }
+  }
+
+  for(var i in l){
+    print(i);
+  }
+}
+
+
+
